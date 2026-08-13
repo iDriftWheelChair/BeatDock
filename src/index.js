79f09a01c4e6eae4a4274b460608be43e77a9d83
iDriftWheelChair <arcanedrive404@gmail.com>
2026-08-13 21:24:10 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
 const { LavalinkManager } = require('lavalink-client');
 const LanguageManager = require('./LanguageManager');
