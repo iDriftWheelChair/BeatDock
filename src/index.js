04bdd397444dc9461cff026be3f4b222a6768b95
iDriftWheelChair <arcanedrive404@gmail.com>
2026-08-13 21:08:39 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const fs = require('fs');
 const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
 const { LavalinkManager } = require('lavalink-client');
